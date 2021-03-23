a070ce96301315406ed0e2ebe9fcbf23ab7673ad
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-23 22:10:32 +0100

    added adr plus why using pouchdb

--- components/Header.vue
+++ components/Header.vue
@@ -74,7 +74,7 @@
             href="#"
             class="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            New Project
+            Publish
           </a>
         </div>
       </div>
---
+++ doc/adr/0002-pouchdb.md
@@ -0,0 +1,23 @@
+# 2. pouchdb
+
+Date: 2021-03-23
+
+## Status
+
+Accepted
+
+## Context
+
+We need a storage system that allows us to:
+
+- Save objects in a JS way
+- Perform different kinds of queries based on indexes
+- Based on browser for higher support
+
+## Decision
+
+Use [pouchdb](https://pouchdb.com/).
+
+## Consequences
+
+Unknown yet
