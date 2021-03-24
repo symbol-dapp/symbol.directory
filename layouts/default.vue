8e89f1c894ef7f1ba92ec5e0bb6779158642794d
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-24 23:13:01 +0100

    publish

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="bg-gray-100">
+  <div class="bg-gray-100 ">
     <Header />
-    <Nuxt />
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-10">
+      <Nuxt />
+    </div>
   </div>
 </template>
