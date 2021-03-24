9b4346500b7f9f21dbd973778678d87de6a619fa
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-25 00:01:22 +0100

    fix header

--- components/Header.vue
+++ components/Header.vue
@@ -13,7 +13,7 @@
             <NuxtLink to="/">
               <img
                 class="block h-8 w-auto"
-                src="images/symbol.directory.logo.small.png"
+                src="/images/symbol.directory.logo.small.png"
                 alt="symbol.directory logo"
               >
               <span />
