1db543ade5000e95d4d527d6c65388a8abef2627
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-25 00:06:44 +0100

    submit

--- components/Header.vue
+++ components/Header.vue
@@ -60,7 +60,7 @@
           class="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden"
         >
           <!-- Mobile menu button -->
-          <NuxtLink to="/Submit">
+          <NuxtLink to="/submit">
             <button
               type="button"
               class="-mx-2 rounded-md p-2 inline-flex items-center justify-center bg-indigo-600 hover:bg-indigo-100 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -74,7 +74,7 @@
           class="hidden lg:flex lg:items-center lg:justify-end xl:col-span-3"
         >
           <NuxtLink
-            to="/Submit"
+            to="/submit"
             class="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Submit
