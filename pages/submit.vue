df9229161a46f9a63b269068fa6e9fe551ef48f3
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-28 19:11:48 +0200

    Basic Info

--- pages/submit.vue
+++ pages/submit.vue
@@ -1,28 +1,39 @@
 <template>
   <div>
-    <div>
-      <div class="text-center">
-        <h1 class="text-5xl mb-10">
-          Submit your project
-        </h1>
+    <form>
+      <div>
+        <div class="text-center">
+          <h1 class="text-5xl mb-10">
+            Submit your project
+          </h1>
+        </div>
+        <SubmitBasicInfo />
       </div>
-      <SubmitBasicInfo />
-    </div>
 
-    <div class="hidden sm:block" aria-hidden="true">
-      <div class="py-5">
-        <div class="border-t border-gray-200" />
+      <div class="hidden sm:block" aria-hidden="true">
+        <div class="py-5">
+          <div class="border-t border-gray-200" />
+        </div>
       </div>
-    </div>
 
-    <SubmitTokenInfo />
+      <SubmitTokenInfo />
 
-    <div class="hidden sm:block" aria-hidden="true">
-      <div class="py-5">
-        <div class="border-t border-gray-200" />
+      <div class="hidden sm:block" aria-hidden="true">
+        <div class="py-5">
+          <div class="border-t border-gray-200" />
+        </div>
       </div>
-    </div>
 
-    <SubmitSocialMedia />
+      <SubmitSocialMedia />
+
+      <div class="text-right mt-5">
+        <button
+          type="submit"
+          class="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Continue
+        </button>
+      </div>
+    </form>
   </div>
 </template>
--- test/components/BasicInfo.spec.ts
+++ test/components/BasicInfo.spec.ts
@@ -6,5 +6,33 @@ describe("BasicInfo", () => {
     const { getByText, getByTestId, emitted } = render(BasicInfo);
 
     getByText("Basic Information");
+
+    const nameInput = getByTestId("name");
+    const websiteInput = getByTestId("website");
+    const typeInput = getByTestId("type");
+    const categoryInput = getByTestId("category");
+    const shortDescriptionInput = getByTestId("shortDescription");
+    const longDescriptionInput = getByTestId("longDescription");
+
+    await fireEvent.update(nameInput, "symbol.directory");
+    await fireEvent.update(websiteInput, "https://symbol.directory");
+    await fireEvent.update(typeInput, "DAPP");
+    await fireEvent.update(categoryInput, "marketplace");
+    await fireEvent.update(shortDescriptionInput, "List all Symbol Projects");
+    await fireEvent.update(
+      longDescriptionInput,
+      "A project to find any project related to Symbol Blockchain"
+    );
+
+    expect(emitted().basicInfo).toBeTruthy();
+    expect(emitted().basicInfo[0][0]).toStrictEqual({
+      name: "symbol.directory",
+      website: "https://symbol.directory",
+      type: "DAPP",
+      category: "marketplace",
+      shortDescription: "List all Symbol Projects",
+      longDescription:
+        "A project to find any project related to Symbol Blockchain"
+    });
   });
 });
