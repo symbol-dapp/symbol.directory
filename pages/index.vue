1eaea9234af8cff5bb408639dce20d4d58690fca
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-28 22:05:37 +0200

    warning

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="py-40 text-center">
+  <div class="py-40">
     <div>
       <Logo />
-      <h1 class="text-5xl mt-5">
+      <h1 class="text-5xl mt-5 text-center">
         symbol.directory
       </h1>
+      <div class="flex justify-center">
+        <div class="rounded-md bg-yellow-50 p-4 mt-10 w-6/12">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <!-- Heroicon name: solid/exclamation -->
+              <svg
+                class="h-5 w-5 text-yellow-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="ml-3">
+              <h3 class="text-sm font-medium text-yellow-800">
+                Attention needed
+              </h3>
+              <div class="mt-2 text-sm text-yellow-700">
+                <p>
+                  This site is under heavy development. <span class="font-bold">Do not use yet </span>. I will publish more information at my Twitter.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
