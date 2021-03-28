351f67cf491b6d50545b835512bd72380ae63684
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-28 21:56:28 +0200

    verify google site

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name:"google-site-verification", content: "OXjgHcYxJnB9W9iIcEGLa0vAKvXgK-zRT-PneknKUKQ" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
