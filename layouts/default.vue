b776cd47211364cf867ed1b2f689b424a8597bce
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-03-30 18:45:52 +0200

    do not show header until complete

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="bg-gray-100 ">
-    <Header />
+    <Header v-if="enabled" />
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-10">
       <Nuxt />
     </div>
     <Footer />
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: function() {
+    return {
+      enabled: false
+    }
+  },
+  mounted: function() {
+    if(this.$route.query.enabled) {
+      this.$data.enabled = true;
+    }
+  }
+})
+</script>
