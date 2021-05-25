9df22a9658a699e20e90b9d714b624233e4485df
aleixmorgadas <aleixmorgadas@users.noreply.github.com>
2021-05-26 00:18:54 +0200

    Using @symbol-dapp/core 0.0.9

--- pages/submit/review.vue
+++ pages/submit/review.vue
@@ -1,24 +1,17 @@
 <template>
   <div>
-    <p>{{ data }}</p>
+    <p>Scan and sign the transaction to list your project</p>
     <div>
       <img :src="transaction" />
-    </div>
-    <div>
-      {{ transaction }}
     </div>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import {
-  Deadline,
-  Message,
   NetworkType,
-  PlainMessage,
-  TransferTransaction
 } from "symbol-sdk";
-import { QRCodeGenerator, TransactionQR } from "symbol-qr-library";
+import { QRCodeGenerator } from "symbol-qr-library";
 import { mapState } from "vuex";
 import { CreateProjectCommand } from "~/models/project/CreateProjectCommand";
 
@@ -35,7 +28,7 @@ export default Vue.extend({
         NetworkType.TEST_NET
       );
       return JSON.stringify(
-        command.toTransaction(1573430400, NetworkType.TEST_NET)
+        command.toTransaction(1615853185, NetworkType.TEST_NET).serialize()
       );
     }
   }),
@@ -44,12 +37,19 @@ export default Vue.extend({
       this.$store.state.submit,
       NetworkType.TEST_NET
     );
-    const transaction = command.toTransaction(1573430400, NetworkType.TEST_NET);
+    const transaction = command.toTransaction(1615853185, NetworkType.TEST_NET);
 
-    const qrCode = QRCodeGenerator.createTransactionRequest(transaction, NetworkType.TEST_NET, '3B5E1FA6445653C971A50687E75E6D09FB30481055E3990C84B25E9222DC1155');
-    qrCode.toBase64().toPromise().then((qrCodeBase64) => {
-      this.transaction = qrCodeBase64;
-    })
+    const qrCode = QRCodeGenerator.createTransactionRequest(
+      transaction,
+      NetworkType.TEST_NET,
+      "3B5E1FA6445653C971A50687E75E6D09FB30481055E3990C84B25E9222DC1155"
+    );
+    qrCode
+      .toBase64()
+      .toPromise()
+      .then(qrCodeBase64 => {
+        this.transaction = qrCodeBase64;
+      });
   }
 });
 </script>
