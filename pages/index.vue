ee133046dd6cf5823010a614afca9026558c0ff8
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-26 01:11:49 +0200

    some UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="py-4">
     <header>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold leading-tight text-gray-900">Project List</h1>
+      <div class="max-w-7xl mx-auto">
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">
+          Project List
+        </h1>
       </div>
     </header>
     <ProjectDisplay v-for="project in projects" :key="project.state.name" :project-state="project.state" />
