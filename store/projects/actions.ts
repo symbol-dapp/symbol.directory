73e2147386af5e8aa713326e013045879740312d
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-26 00:49:54 +0200

    using a secure node

--- store/projects/actions.ts
+++ store/projects/actions.ts
@@ -19,7 +19,7 @@ import { Commit } from 'vuex';
 import { CreateProjectCommand } from '~/models/project/CreateProjectCommand';
 import { ProjectState } from '~/models/project/Project';
 import { ProjectJournalResolver } from '~/models/project/ProjectJournalResolver';
-const nodeUrl = 'http://ngl-dual-001.testnet.symboldev.network:3000';
+const nodeUrl = 'https://ngl-api-001.testnet.symboldev.network:3001/node/info';
 const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
 const transactionHttp = repositoryFactory.createTransactionRepository();
 
