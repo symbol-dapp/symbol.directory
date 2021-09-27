1bd2ca10e77a948c641f6677d1c56eaff90259d9
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-27 13:21:58 +0200

    gen prod

--- services/NetworkTypeResolver.ts
+++ services/NetworkTypeResolver.ts
@@ -1,6 +1,7 @@
 import { NetworkType } from 'symbol-sdk';
 
 const NetworkTypeResolver = () => {
+  console.log('env', process.env.PRODUCTION);
   if (process.env.PRODUCTION === 'true') {
     return NetworkType.MAIN_NET;
   }
