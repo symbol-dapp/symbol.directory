4e07bdd3eef376ae6c51baceca50ebb67216f054
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-27 13:17:33 +0200

    Main Node

--- services/RepositoryFacade.ts
+++ services/RepositoryFacade.ts
@@ -1,8 +1,17 @@
-import { RepositoryFactoryHttp } from 'symbol-sdk';
+import { NetworkType, RepositoryFactoryHttp } from 'symbol-sdk';
+import NetworkTypeResolver from './NetworkTypeResolver';
 
-const nodeUrl = 'https://ngl-api-001.testnet.symboldev.network:3001';
-export const HTTPRepositoryFactory = new RepositoryFactoryHttp(nodeUrl);
-export const WSSRepositoryFactory = new RepositoryFactoryHttp(nodeUrl, {
-  websocketUrl: 'wss://ngl-api-001.testnet.symboldev.network:3001/ws',
+const nodeHttpsURLMainNet = 'https://xym.jp1.node.leywapool.com:3001';
+const nodeWssURLMainNet = 'wss://xym.jp1.node.leywapool.com:3001/ws';
+
+const nodeHttpsURLTestNet = 'https://ngl-api-001.testnet.symboldev.network:3001';
+const nodeWssURLTestNet = 'wss://ngl-api-001.testnet.symboldev.network:3001/ws';
+
+const nodeHttpURL = NetworkTypeResolver() === NetworkType.MAIN_NET ? nodeHttpsURLMainNet : nodeHttpsURLTestNet;
+const nodeWssURL = NetworkTypeResolver() === NetworkType.MAIN_NET ? nodeWssURLMainNet : nodeWssURLTestNet;
+
+export const HTTPRepositoryFactory = new RepositoryFactoryHttp(nodeHttpURL);
+export const WSSRepositoryFactory = new RepositoryFactoryHttp(nodeHttpURL, {
+  websocketUrl: nodeWssURL,
   websocketInjected: WebSocket
 });
