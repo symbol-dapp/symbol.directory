68e0c4142514eafd5d2c2856789430dec5f8ba69
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-10-11 17:16:52 +0200

    Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'symbol.directory',
+    title: 'Symbol Directory',
     htmlAttrs: {
       lang: 'en'
     },
