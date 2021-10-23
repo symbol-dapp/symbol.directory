0365571f258d7cfa5a154f5700e2d883fa6fe1df
Klim Geran <klimgeran@mail.ru>
2021-10-23 20:39:39 +0800

    Create ru.js
    Added translation into Russian for the site https://symbol.directory based on the en.js file

---
+++ lang/ru.js
@@ -0,0 +1,50 @@
+export default {
+  search: 'Поиск',
+  submit: 'Представить',
+  name: 'Имя',
+  type: 'Тип',
+  category: 'Категория',
+  description: 'Описание',
+  rating: 'Рейтинг',
+  notRated: 'Еще не оценен',
+  website: 'Cайт',
+  metrics: 'Метрики',
+  submitForm: {
+    projectInfo: 'Информация о проекте',
+    shortDescription: 'Краткое описание',
+    shortDescriptionPlaceholder: 'Это должно дать представление пользователям о том, что делает ваш продукт',
+    selectType: 'Выберите тип',
+    selectCategory: 'Выберите категорию',
+    websitePlaceholder: 'URL ссылка, для посещения сайта вашего продукта',
+    detailDescription: 'Подробное Описание',
+    detailDescriptionPlaceholder: 'Подробное описание, лучше расскажет о ваших продуктах, для аудитории. Наши пользователи увидят подробную информацию о вашем проекте, на специальной отдельной странице.'
+  },
+  types: {
+    application: 'Приложение',
+    company: 'Компания',
+    dapp: 'Децентрализованное приложение Dapp',
+    defi: 'Децентрализованные финансы DeFi',
+    nft: 'NFT',
+    node: 'Нода',
+    protocol: 'Протокол',
+    education: 'Образование',
+    tools: 'Инструменты',
+    wallet: 'Кошелек',
+    team: 'Команда'
+  },
+  categories: {
+    media: 'СМИ',
+    wallet: 'Кошелек',
+    priceTracker: 'Отслеживание цен',
+    jobSearch: 'Поиск работы',
+    game: 'Игра',
+    gambling: 'Гемблинг',
+    finance: 'Финансы',
+    exchange: 'Биржа',
+    social: 'Социальный',
+    marketplace: 'Маркетплейс',
+    utilities: 'Утилиты',
+    others: 'Другое',
+    highRisk: 'Высокий риск'
+  }
+};
